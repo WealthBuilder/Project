e0de326edf6f5186e2f995bc24a4aef0ae275f88
jayander <jayander@gmail.com>
2015-05-25 15:00:21 +0530

    Initial version

---
+++ WealthBuilder/src/com/wealth/builder/test/Test.java
@@ -0,0 +1,10 @@
+package com.wealth.builder.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first class which will help me make 1 crore rupees.");
+
+	}
+
+}
